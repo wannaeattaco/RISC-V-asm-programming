ed854de52ffb2e21be531e421f11696413b22752
Thunyanan Tangpipatpong <thunynan.t@ku.th>
2024-03-04 14:53:59 +0700

    add file

--- print_array.s
+++ print_array.s
@@ -1,59 +1,55 @@
 .data
 A: .word 11, 22, 33, 44, 55
-space: .asciiz " "
-newline: .asciiz "\n"
+B: .word 11, 22, 33, 44, 55, 66, 77, 88, 99, 1234
+newline: .string "\n"
+space: .string " "
 
 .text
 main:
-
-    # passing the two arguments to a0 and a1
-    la a0, A
-    addi a1, x0, 5
-
-    # jump to the print_array procedure
+    la a0, B # loading the starting address of array A to a0
+    addi a1, x0, 10 # passing the array size value to a1
     jal print_array
-
-    # exit gracefully
+    
+    # exit cleanly
     addi a0, x0, 10
-    addi a1, x0, 0
-    ecall # Terminate ecall
+    ecall
 
 print_array:
-    addi sp, sp, -4
-    sw ra, 0(sp) # save ra register on to the stack
-    addi t0, x0, 0 # i = 0
-loop:
-    bge t0, a1, loop_exit # exit loop if i >= size
-    slli t1, t0, 2 # i = i * 4
-    add t1, a0, t1 # t1 = &A[0] + i*4
-    lw t1, 0(t1) # t1 = A[i]
-
-    # save a0 and a1 on to the stack
+    addi t0, x0, 0 # let the i value be in t0
+loop1:
+    bge t0, a1, exit1
+    slli t1, t0, 2 # t1 has the i*4 value
+    add t2, t1, a0
+    lw t3, 0(t2) # t3 has the value of A[i]
+    
+    # print A[i]
+    
+    # save a0 and a1 on to the stack; caller save as ecall realizes that a0 and a1 needed after the call
     addi sp, sp, -8
-    sw a0, 4(sp)
-    sw a1, 0(sp)
-
-    # printf("%d ", A[i])
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    
     addi a0, x0, 1
-    mv a1, t1
+    mv a1, t3
     ecall
+
+    # print space
     addi a0, x0, 4
     la a1, space
     ecall
-
-    # restore a0 and a1 from the stack
-    lw a0, 4(sp)
-    lw a1, 0(sp)
+    
+    # restoring a0 and a1 to their original value
+    lw a0, 0(sp)
+    lw a1, 4(sp)
     addi sp, sp, 8
-
+    
     addi t0, t0, 1 # i++
-    j loop # goto loop
-loop_exit:
+    j loop1
+    
+exit1:
+    # print newline
     addi a0, x0, 4
     la a1, newline
     ecall
-
-    # restore ra and return
-    lw ra, 0(sp)
-    addi sp, sp, 4
+    
     jr ra
